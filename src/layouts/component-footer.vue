faa273523ef408be85f1bdff51e28ae55f972019
rachary <rachmad.ary1@gmail.com>
2024-07-17 19:01:15 +0700

    adding footer

--- src/layouts/component-footer.vue
+++ src/layouts/component-footer.vue
@@ -5,9 +5,7 @@
     <div
       class="relative bottom-0 w-full border-t border-primary-4 border-opacity-50 py-5 text-center md:py-10"
     >
-      <h2 class="text-xs font-bold md:text-2xl">
-        &copy; Lovelia. All right reserved.
-      </h2>
+      <h1 class="content__main">&copy; Lovelia. All right reserved.</h1>
     </div>
   </main>
 </template>
