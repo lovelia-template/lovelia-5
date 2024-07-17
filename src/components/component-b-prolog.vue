38e772070b8f2cd4d8497eb7b8a0d8a0ca03d38f
rachary <rachmad.ary1@gmail.com>
2024-07-17 19:00:29 +0700

    adding component prolog

--- src/components/component-b-prolog.vue
+++ src/components/component-b-prolog.vue
@@ -7,7 +7,7 @@
     >
       <div class="mb-7 flex w-full items-center gap-16 opacity-50">
         <span class="block h-[1px] w-full bg-primary-1"></span>
-        <p>Prologue</p>
+        <p class="content__sub">Prologue</p>
       </div>
       <div class="relative flex h-[144px] items-center justify-center">
         <img
@@ -15,24 +15,26 @@
           class="absolute h-[202px] w-full max-w-[284px] object-cover object-center"
           alt=""
         />
-        <h1 class="max-w-[527px] text-center">
+        <h1 class="heading__title max-w-[527px] text-center">
           “Ketika dua hati bersatu, sebuah kisah cinta abadi terukir. Bersama,
           mari kita rayakan cinta dan kebahagiaan kami”
         </h1>
       </div>
-      <p class="text-left text-primary-3 md:text-center">Revida & Albert</p>
+      <p class="content__sub text-left text-primary-3 md:text-center">
+        Revida & Albert
+      </p>
     </div>
     <div
       class="flex w-full flex-col items-center justify-center gap-10 py-10 text-center text-primary-1 opacity-50 md:py-20"
     >
-      <p class="max-w-[526px] text-center">
+      <p class="content__sub max-w-[526px] text-center">
         Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan
         pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan
         merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan
         sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda
         (kebesaran Allah) bagi kaum yang berpikir.
       </p>
-      <p class="font-semibold">QS. Ar-Rum Ayat 21</p>
+      <p class="content__sub font-semibold">QS. Ar-Rum Ayat 21</p>
     </div>
   </div>
 </template>
