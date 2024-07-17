5e85da0d295e5751e103689a56d75e0956123607
rachary <rachmad.ary1@gmail.com>
2024-07-17 21:40:07 +0700

    update heading

--- src/views/component-heading.vue
+++ src/views/component-heading.vue
@@ -8,7 +8,7 @@
 <template>
   <div class="container flex justify-center py-20">
     <div
-      class="relative flex h-[202px] w-full flex-col items-center justify-end md:justify-center"
+      class="relative flex h-[202px] w-full flex-col items-center justify-center md:justify-center"
     >
       <img
         src="/images/heading.png"
@@ -16,15 +16,15 @@
         alt=""
       />
       <div
-        class="relative z-10 flex h-full max-h-[127px] flex-col items-center justify-between pb-2 md:max-h-[156px]"
+        class="relative z-10 flex h-full max-h-[137px] translate-y-5 flex-col items-center justify-between pb-2 md:max-h-[156px]"
       >
-        <h1 class="heading__title text-center capitalize">
+        <h2 class="heading__subtitle capitalize opacity-50">
+          {{ props.subtittle }}
+        </h2>
+        <h1 class="font-miama text-center text-4xl capitalize leading-[50px]">
           {{ props.tittle }}
           <span class="block">{{ props.sub }}</span>
         </h1>
-        <h2 class="heading__subtitle capitalizeopacity-50">
-          {{ props.subtittle }}
-        </h2>
       </div>
     </div>
   </div>
